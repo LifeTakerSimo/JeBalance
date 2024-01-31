c0e8e48dddfb89bf36f66f7cbfa7a54195673405
LifeTakerSimo <Milani.taker@hotmail.com>
2024-01-31 20:47:22 +0100

    index change

--- Interface/Pages/Index.cshtml.cs
+++ Interface/Pages/Index.cshtml.cs
@@ -5,7 +5,6 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Domain.Model;
 using System.Diagnostics;
 
 public class IndexModel : PageModel
@@ -18,7 +17,10 @@ public class IndexModel : PageModel
         _httpClient = httpClient;
     }
 
-    public async Task<IActionResult> OnPostCreateDenunciationAsync(string informateurFirstName, string informateurLastName, string countryEvasion, string typeOfOffense)
+    public async Task<IActionResult> OnPostCreateDenunciationAsync(
+        string informateurFirstName, string informateurLastName, string informateurStreetName, string informateurStreetNumber, string informateurPostalCode, string informateurCityName, string informateurEmail, string informateurUsername,
+        string suspectFirstName, string suspectLastName, string suspectStreetName, string suspectStreetNumber, string suspectPostalCode, string suspectCityName, string suspectEmail,
+        string countryEvasion, string typeOfOffense)
     {
         var denonciation = new
         {
@@ -26,9 +28,22 @@ public class IndexModel : PageModel
             {
                 FirstName = informateurFirstName,
                 LastName = informateurLastName,
+                StreetName = informateurStreetName,
+                StreetNumber = informateurStreetNumber,
+                PostalCode = informateurPostalCode,
+                CityName = informateurCityName,
+                Email = informateurEmail,
+                UserName = informateurUsername,
             },
             Suspect = new
             {
+                FirstName = suspectFirstName,
+                LastName = suspectLastName,
+                StreetName = suspectStreetName,
+                StreetNumber = suspectStreetNumber,
+                PostalCode = suspectPostalCode,
+                CityName = suspectCityName,
+                Email = suspectEmail,
             },
             Offense = typeOfOffense,
             EvasionCountry = countryEvasion
@@ -40,12 +55,12 @@ public class IndexModel : PageModel
 
         if (response.IsSuccessStatusCode)
         {
-            Debug.WriteLine($"Informateur: {informateurFirstName} {informateurLastName}, Country of Evasion: {countryEvasion}, Type of Offense: {typeOfOffense}");
+            Debug.WriteLine($"Denunciation created successfully.");
             return Content("The denunciation was received successfully.");
         }
         else
         {
-            ModelState.AddModelError(string.Empty, "There was an error creating the denonciation.");
+            ModelState.AddModelError(string.Empty, "There was an error creating the denunciation.");
             return Page();
         }
     }
